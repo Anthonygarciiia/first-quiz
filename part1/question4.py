326621ef424624dead983ccfa7e389214bb40a49
Anthony Garcia <anthonygarciiia@icloud.com>
2023-11-05 10:49:21 -0500

    Punto 4

--- part1/question4.py
+++ part1/question4.py
@@ -10,39 +10,50 @@ import pets_db
 #  Question 4
 ################################################################################
 #
-# Instructions:
-# Question 4 and Question 5 are about writing SQL. THey use the database that is 
-# created in the file `pets_db.py`. 
-# These questions use a database called SQLite. You do not need to install anything.
-# In the file `pets_db.py` three tables are created. Data is then added to each 
-# of the tables. The questions below are about how the data in each of the tables
-# is related.
+# Instrucciones:
+# Pregunta 4 y Pregunta 5 tratan de escribir SQL.Usan la base de datos que es
+# Creado en el archivo `Pets_db.py`.
+# Estas preguntas utilizan una base de datos llamada SQLite.No necesita instalar nada.
+# En el archivo se crean tres tablas.Luego se agregan datos a cada uno
+# de las tablas.Las preguntas a continuación son sobre cómo los datos en cada una de las tablas
+# está relacionado.
 
-# Part 4.A:
-# Write SQL to select the pets that are owned by nobody.
-# The output should be a list of tuples in the format: (<pet name>, <species>, <age>)
+# Parte 4.A:
+# Escribe SQL para seleccionar las mascotas que son propiedad de nadie.
+# La salida debe ser una lista de tuplas en el formato: (<nombre de mascota>, <pecies>, <ge>)
 
 sql_pets_owned_by_nobody = """
-
-Your SQL here.
-
+SELECT a.name, a.species, a.age
+FROM animals a
+LEFT JOIN people_animals pa ON a.animal_id = pa.pet_id
+WHERE pa.owner_id IS NULL;
 """
 
 # Part 4.B:
-# Write SQL to select how the number of pets are older than their owners. 
-# The output should be an integer.
+# Escriba SQL para seleccionar cómo el número de mascotas es más antigua que sus dueños.
+# La salida debe ser un entero.
 
 sql_pets_older_than_owner = """
-
-Your SQL here.
-
+SELECT COUNT(*) AS num_pets_older_than_owners
+FROM people_animals pa
+JOIN animals a ON pa.pet_id = a.animal_id
+JOIN people p ON pa.owner_id = p.person_id
+WHERE a.age > p.age;
 """
 
-# Part 4.C: BONUS CHALLENGE! 
-# Write SQL to select the pets that are owned by Bessie and nobody else.
-# The output should be a list of tuples in the format: (<person name>, <pet name>, <species>)
+# Part 4.C: ¡Desafío de bonificación!
+# Escribe SQL para seleccionar las mascotas que son propiedad de Bessie y nadie más.
+# La salida debe ser una lista de tuplas en el formato: (<nombre de la persona>, <nombre de mascota>, <pecies>)
 sql_only_owned_by_bessie = """ 
-
-Your SQL here.
-
+SELECT p.name AS person_name, a.name AS pet_name, a.species
+FROM people p
+JOIN people_animals pa ON p.person_id = pa.owner_id
+JOIN animals a ON pa.pet_id = a.animal_id
+WHERE p.name = 'bessie'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM people_animals pa_other
+    WHERE pa_other.pet_id = pa.pet_id
+      AND pa_other.owner_id != p.person_id
+  );
 """
