a752aa26df850970b3ba1908386bc70429035432
Anthony Garcia <anthonygarciiia@icloud.com>
2023-11-05 10:23:54 -0500

    Punto 3

--- part1/question3.py
+++ part1/question3.py
@@ -8,37 +8,93 @@
 #  Question 3
 ################################################################################
 #
-# Instructions:
-# Make a Python class for a magical oven that can combine ingredients at 
-# different temperatures to craft special materials.
-# 
-# The oven class should have the methods:
-# - add(item) to add an oven to be combined
-# - freeze() to freeze the ingredients
-# - boil() to boil the ingredients
-# - wait() to combine the ingredients with no change in temperature
-# - get_output() to get the result 
+# Instrucciones:
+# Haz una clase de Python para un horno mágico que pueda combinar ingredientes en
+# diferentes temperaturas para elaborar materiales especiales.
 #
-# You will need to change the `make_oven()` function to return a new instance
-# of your oven.
+# La clase de horno debe tener los métodos:
+# - Agregar (elemento) para agregar un horno para combinar
+# - congelarse () para congelar los ingredientes
+# - hervir () para hervir los ingredientes
+# - espera () para combinar los ingredientes sin cambios en la temperatura
+# - get_output () para obtener el resultado
 #
-# The `alchemy_combine()` function will use your oven. You can see the expected 
-# formulas and their outputs in the test file, `question3_test.py`.
+# Deberá cambiar la función `make_oven ()` para devolver una nueva instancia
+# de tu horno.
+#
+# La función `alchemy_combine ()` usará su horno.Puedes ver la esperada
+# Fórmulas y sus salidas en el archivo de prueba, `pregunta3_test.py`.
 
-# This function should return an oven instance!
+# ¡Esta función debería devolver una instancia de horno!
+
+class MagicalOven:
+    def __init__(self):
+        self.ingredients = []
+        self.output = None
+
+    def add(self, element):
+        self.ingredients.append(element)
+
+    def freeze(self):
+        for ingredient in self.ingredients:
+            if ingredient == "cheese" or ingredient == "dough" or ingredient == "tomato":
+                # Cambiar ingredientes a "frozen ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "frozen " + ingredient
+                self.output = "pizza"  # Establecer la salida en "pizza"
+        if ingredient == "water" or ingredient == "air":
+                # Cambiar ingredientes a "frozen ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "frozen " + ingredient
+                self.output = "snow"  # Establecer la salida en "snow"
+        if ingredient == "lead" or ingredient == "mercury":
+                # Cambiar ingredientes a "frozen ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "frozen " + ingredient
+                self.output = "gold"  # Establecer la salida en "gold"
+    def boil(self):
+        for ingredient in self.ingredients:
+            if ingredient == "cheese" or ingredient == "dough" or ingredient == "tomato":
+                # Cambiar ingredientes a "boiled ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "boiled " + ingredient
+                self.output = "pizza"  # Establecer la salida en "pizza"
+            if ingredient == "water" or ingredient == "air":
+                # Cambiar ingredientes a "boiled ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "boiled " + ingredient
+                self.output = "snow"  # Establecer la salida en "snow"
+            if ingredient == "lead" or ingredient == "mercury":
+                # Cambiar ingredientes a "frozen ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "frozen " + ingredient
+                self.output = "gold"  # Establecer la salida en "gold"
+
+    def wait(self):
+        for ingredient in self.ingredients:
+            if ingredient == "cheese" or ingredient == "dough" or ingredient == "tomato":
+                # Cambiar ingredientes a "mixed ingredients"
+                self.ingredients[self.ingredients.index(ingredient)] = "mixed ingredients"
+                self.output = "pizza"  # Establecer la salida en "pizza"
+            if ingredient == "water" or ingredient == "air":
+                # Cambiar ingredientes a "boiled ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "boiled " + ingredient
+                self.output = "snow"  # Establecer la salida en "snow"
+            if ingredient == "lead" or ingredient == "mercury":
+                # Cambiar ingredientes a "frozen ingredient"
+                self.ingredients[self.ingredients.index(ingredient)] = "frozen " + ingredient
+                self.output = "gold"  # Establecer la salida en "gold"
+       
+
+    def get_output(self):
+        return self.output
+
 def make_oven():
-  None
+    return MagicalOven()
 
 def alchemy_combine(oven, ingredients, temperature):
-  
-  for item in ingredients:
-    oven.add(item)
+    for item in ingredients:
+        oven.add(item)
 
-  if temperature < 0:
-    oven.freeze()
-  elif temperature >= 100:
-    oven.boil()
-  else:
-    oven.wait()
+    if temperature < 0:
+        oven.freeze()
+    elif temperature >= 100:
+        oven.boil()
+    else:
+        oven.wait()
 
-  return oven.get_output()
+    return oven.get_output()
